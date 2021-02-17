2e92f7cf76d2c5a09b13afdff729a371f8b39eae
WillisJackson <jacksonw005@deltalearns.ca>
2021-02-17 10:57:40 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -16,6 +16,32 @@ Access granted
 Access denied
 """
 
+"""
+we are going to be checking the value of the variable "name"
+before we have input that creates it, so we should create
+a variable with no contents otherwise the conditional statement
+might throw an error stating that the variable does not exist
+
+Structure:
+
+create a username and password variable
+use while loop to repeat the following block if the password is not correct:
+    enter the username
+    enter the password
+    check to see if the username and password are both correct
+        print Access granted if they're both correct
+        print Access denied if at least one is wrong
+
+
+"""
+
+
+username = ""
+password = ""
+
+
+
+
 username=input("Enter a Username: ")
 password=input("Enter a Password: ")
 username=str(username)
